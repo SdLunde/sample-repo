cbdb34be826b3540c3afcebe1653eeb4f63497f4
SdLunde <sindre.djupevik.lunde@gmail.com>
2021-02-14 13:33:14 +0100

    added git comments

--- hello_world.py
+++ hello_world.py
@@ -14,3 +14,6 @@ print(greet("Sindre"))
 # Sometimes it throws errors when using pip and installing modules globally not locally in venv
 # C:\Users\sindr\OneDrive\Skrivebord\vscode\venv\Scripts\python.exe -m pip install --upgrade pip
 # c:/Users/sindr/OneDrive/Skrivebord/vscode/venv/Scripts/python.exe -m pip install requests
+# git config --global user.name "SdLunde"
+# git config --global user.email "sindre.djupevik.lunde@gmail.com"
+# git config --list
